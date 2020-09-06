87bd9bd7ee4f30c24290dd036954526f46938e0e
Aleksi Anttila <aleant@wapice.com>
2020-09-06 08:08:38 +0300

    Updates

---
+++ precache-manifest.24000fff50f81fc8847b53f24a5fcc9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0073debbf3e9f0628b5a14e46252957d",
+    "url": "/tut-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "57a20786391cfbf0c357",
+    "url": "/tut-monsters-rolodex/static/css/main.559a9df8.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/tut-monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tut-monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a20786391cfbf0c357",
+    "url": "/tut-monsters-rolodex/static/js/main.c602a79f.chunk.js"
+  },
+  {
+    "revision": "906262755332b6a33331",
+    "url": "/tut-monsters-rolodex/static/js/runtime-main.d9ba0fe6.js"
+  }
+]);
